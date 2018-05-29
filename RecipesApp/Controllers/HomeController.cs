f3ed7e7d390996b64310a013056d367162339e8c
AntonD <band_eros_87@mail.ru>
2018-05-29 12:15:00 +0300

    1) Added method for get string of type course 2) Create list for ingridient 3) In create view change SelectList on DropDownList 4) Added Editor (without functionally)

--- RecipesApp/Controllers/HomeController.cs
+++ RecipesApp/Controllers/HomeController.cs
@@ -47,16 +47,7 @@ namespace RecipesApp.Controllers
             s += "Спосбо приготовления: " + rec.Cooking;
             s += "Фото: " + rec.Image;
 
-            switch (rec.Type)
-            {
-                case "Суп": rec.Type = "Soup"; break;
-                case "Второе блюдо": rec.Type = "SecondCourse"; break;
-                case "Салат": rec.Type = "Salad"; break;
-                case "Закуска": rec.Type = "Snak"; break;
-                case "Выпечка": rec.Type = "Bake"; break;
-                case "Десерт": rec.Type = "Dessert"; break;
-                case "Напиток": rec.Type = "Drink"; break;
-            }
+            rec.Type = GetTypeCourse(rec.Type);
 
             db.Recipes.Add(rec);
 
@@ -67,6 +58,26 @@ namespace RecipesApp.Controllers
             Response.Redirect(s);
 
             return s;
+        }
+
+        private string GetTypeCourse(string type)
+        {
+            string s = "";
+
+            switch (type)
+            {
+                case "Суп":             s = "Soup"; break;
+                case "Второе блюдо":    s = "SecondCourse"; break;
+                case "Салат":           s = "Salad"; break;
+                case "Закуска":         s = "Snak"; break;
+                case "Выпечка":         s = "Bake"; break;
+                case "Десерт":          s = "Dessert"; break;
+                case "Напиток":         s = "Drink"; break;
+                default:                s = "Soup"; break;
+            }
+
+            return s;
+
         }
 
         public ActionResult About(string id)
